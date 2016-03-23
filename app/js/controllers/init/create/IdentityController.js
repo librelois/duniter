278ece5c457238ff14c9a2541d30bd3539282170
cgeek <cem.moreau@gmail.com>
2016-03-23 11:54:48 +0100

    Change pubkey preview

--- app/js/controllers/init/create/IdentityController.js
+++ app/js/controllers/init/create/IdentityController.js
@@ -2,7 +2,7 @@
 
 var co = require('co');
 
-module.exports = ($scope, BMA) => {
+module.exports = ($scope, $timeout, BMA) => {
 
   setTimeout(() => {
     $('select').material_select();
@@ -15,14 +15,32 @@ module.exports = ($scope, BMA) => {
     }
   };
 
+  let concat = "";
+  $scope.pubkey_preview = "";
+  let timeout = preview();
+
+  function preview() {
+    return $timeout(() => {
+      if ($scope.$parent) {
+        let salt = $scope.$parent.conf.idty_entropy;
+        let pass = $scope.$parent.conf.idty_password;
+        let newConcat = [salt, pass].join('');
+        if (salt && pass && newConcat != concat) {
+          console.log('different');
+          concat = newConcat;
+          $scope.previewPubkey(concat);
+          timeout = preview();
+        } else {
+          timeout = preview();
+        }
+      }
+    }, 100);
+  }
+
   $scope.previewPubkey = () => co(function *() {
     let data = yield BMA.webmin.key.preview({
       conf: $scope.$parent.conf
     });
-    $scope.$parent.pubkey_preview = data.pubkey;
-    let modal = $('#modal-pubkey');
-    if (modal.css('display') == 'none') {
-      $('#modal-pubkey').openModal();
-    }
-  });
+    $scope.pubkey_preview = data.pubkey;
+  }).catch(() => null);
 };
