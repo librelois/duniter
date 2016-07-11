5e262725d59fb1d8645790f55cacc1fded611c54
cgeek <cem.moreau@gmail.com>
2016-07-11 17:18:22 +0200

    Fix duniter/duniter#488 Wallet window now remembers its size

--- app/js/controllers/main/MainController.js
+++ app/js/controllers/main/MainController.js
@@ -15,6 +15,23 @@ module.exports = ($scope, $state, BMA, summary, UIUtils) => {
 
   UIUtils.changeTitle(summary.version);
 
+  $scope.openWallet = () => {
+
+    let walletHeight = parseInt(localStorage.getItem('wallet_height')) || 1000;
+    let walletWidth = parseInt(localStorage.getItem('wallet_width')) || 1400;
+
+    gui.Window.open (window.location.origin + '/cesium/index.html', {
+      position: 'center',
+      height: walletHeight,
+      width: walletWidth
+    }, function(win) {
+      win.on('closed', function() {
+        localStorage.setItem('wallet_height', win.window.innerHeight - 8); // Seems to always have 8 pixels more
+        localStorage.setItem('wallet_width', win.window.innerWidth - 16); // Seems to always have 16 pixels more
+      });
+    });
+  };
+
   $scope.startServer = () => {
     $scope.server_stopped = false;
     return co(function *() {
